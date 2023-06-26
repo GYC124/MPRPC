2b0c4a3bc8192484683889c37942154fe0fc9d1e
GYC124 <15191586286@163.com>
2023-06-26 22:08:25 +0800

    rpc code example

---
+++ test/protobuf/main.cc
@@ -0,0 +1,56 @@
+#include "test.pb.h"
+#include <iostream>
+#include <string>
+using namespace fixbug;
+
+int main()
+{
+    // LoginResponse rsp;
+    // ResultCode *rc = rsp.mutable_result();
+    // rc->set_errcode(1);
+    // rc->set_errmsg("登录处理失败了");
+    
+    GetFriendListsResponse rsp;
+    // mutable_result() 的作用是获取消息对象中指定字段的可变指针，使得我们可以对该字段进行操作，例如设置其内部的字段值
+    ResultCode *rc = rsp.mutable_result();
+    rc->set_errcode(0);
+
+    User *user1 = rsp.add_friend_list();
+    user1->set_name("zhang san");
+    user1->set_age(20);
+    user1->set_sex(User::MAN);
+
+    User *user2 = rsp.add_friend_list();
+    user2->set_name("li si");
+    user2->set_age(22);
+    user2->set_sex(User::MAN);
+
+    std::cout << rsp.friend_list_size() << std::endl;
+
+    return 0;
+}
+
+int main1()
+{
+    // 封装了login请求对象的数据
+    LoginRequest req;
+    req.set_name("zhang san");
+    req.set_pwd("123456");
+
+    // 对象数据序列化 =》 char*  序列化为字符流
+    std::string send_str;
+    if (req.SerializeToString(&send_str))
+    {
+        std::cout << send_str.c_str() << std::endl;
+    }
+
+    // 从send_str反序列化一个login请求对象
+    LoginRequest reqB;
+    if (reqB.ParseFromString(send_str))
+    {
+        std::cout << reqB.name() << std::endl;
+        std::cout << reqB.pwd() << std::endl;
+    }
+
+    return 0;
+}
